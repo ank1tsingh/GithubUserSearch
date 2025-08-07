83b74cc25f66f4e14e544d5ea826aa3b914970ea
Ankit <118796622+ank1tsingh@users.noreply.github.com>
2025-08-08 01:03:33 +0530

    Add: Core Data entity extensions and conversion methods

---
+++ GithubUserSearch/Models/CoreData/CachedRepository+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  CachedRepository+Extensions.swift
+//  GithubUserSearch
+//
+//  Created by Ankit on 07/08/25.
+//
+
+import Foundation
+import CoreData
+
+extension CachedRepository {
+    
+    func toGitHubRepository() -> GitHubRepository {
+        GitHubRepository(
+            id: Int(repositoryID),
+            repositoryName: repositoryName ?? "",
+            fullRepoName: fullRepoName ?? "",
+            repoDescription: repoDescription,
+            starCount: Int(starCount),
+            forkCount: Int(forkCount),
+            primaryLanguage: primaryLanguage,
+            repoURL: repoURL ?? "",
+            lastUpdate: lastUpdate ?? "",
+            isPrivateRepo: isPrivateRepo
+        )
+    }
+    
+    func updateFrom(_ repository: GitHubRepository) {
+        repositoryID = Int32(repository.id)
+        repositoryName = repository.repositoryName
+        fullRepoName = repository.fullRepoName
+        repoDescription = repository.repoDescription
+        starCount = Int32(repository.starCount)
+        forkCount = Int32(repository.forkCount)
+        primaryLanguage = repository.primaryLanguage
+        repoURL = repository.repoURL
+        lastUpdate = repository.lastUpdate
+        isPrivateRepo = repository.isPrivateRepo
+        lastFetched = Date() 
+    }
+}
---
+++ GithubUserSearch/Models/CoreData/CachedUser+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  CachedUser+Extensions.swift
+//  GithubUserSearch
+//
+//  Created by Ankit on 07/08/25.
+//
+
+import Foundation
+import CoreData
+
+extension CachedUser {
+    
+    func toGitHubUser() -> GitHubUser {
+        GitHubUser(
+            id: Int(userID),
+            username: username ?? "",
+            profileImageURL: profileImageURL ?? "",
+            githubURL: githubURL ?? "",
+            displayName: displayName,
+            userBio: userBio,
+            repositoryCount: Int(repositoryCount),
+            followerCount: Int(followerCount),
+            followingCount: Int(followingCount),
+            accountCreated: accountCreated ?? ""
+        )
+        
+    }
+    func updateFrom(_ user: GitHubUser) {
+        userID = Int32(user.id)
+        username = user.username
+        displayName = user.displayName
+        userBio = user.userBio
+        profileImageURL = user.profileImageURL
+        githubURL = user.githubURL
+        repositoryCount = Int32(user.repositoryCount ?? 0)
+        followerCount = Int32(user.followerCount ?? 0)
+        followingCount = Int32(user.followingCount ?? 0)
+        accountCreated = user.accountCreated
+        lastFetched = Date()
+    }
+}
